b87a51de1371bcf93d769d5a3c41fa50ad985cca
HAMADA, Satoshi <tkcs@pelepop.com>
2012-05-09 12:58:56 +0900

    first commit

---
+++ wp_update_timer.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: WP Update Timer
+Plugin URI: http://www.hot-stuff.com/
+Description: Hot Stuffサイト構築用プラグイン
+Author: cinra.dev
+Version: 0.1
+Author URI: http://www.cinra.co.jp/
+*/
+
+// -- init --
+
+
+
+?>
